7243b19723275098dba639f40b5686ed183c29b8
Peter <obidaberechukwu@gmail.com>
2023-12-26 20:20:55 +0100

    worked on the # functions

--- backend/contracts/Escrow.sol
+++ backend/contracts/Escrow.sol
@@ -4,8 +4,9 @@ pragma solidity ^0.8.18;
 contract Escrow {
     address admin;
 
+    uint256[] public transactionIds;
+    uint256 public transactionCount;
     struct Transaction {
-        uint id;
         bool paidStatus;
     	address receiver;
         address sender;
@@ -31,9 +32,33 @@ contract Escrow {
 
     function refundFunds (uint id) public onlyAdmin {}
 
-    function payReciever(uint id) public onlyAdmin {}
+    function payReciever(uint transactionId) public onlyAdmin {
+        require(transactions[transactionId].receiver == msg.sender, "Unauthorized receiver");
+        require(!transactions[transactionId].paidStatus, "Payment already made");
+        require(block.timestamp >= transactions[transactionId].paymentWindow, "Payment window not expired yet");
 
-    function listTransactions() public {}
+        // Perform payment
+        address payable receiver = payable(transactions[transactionId].receiver);
+        uint256 amountToSend = transactions[transactionId].amount;
 
-    function viewAdmin() public {}
+        require(address(this).balance >= amountToSend, "Insufficient contract balance");
+
+        transactions[transactionId].paidStatus = true; // Mark transaction as paid
+        receiver.transfer(amountToSend); // Send the amount to the receiver
+    }
+
+    function listTransactions() public view returns (Transaction[] memory) {
+       Transaction[] memory result = new Transaction[](transactionIds.length);
+
+        for (uint256 i = 0; i < transactionIds.length; i++) {
+            uint256 id = transactionIds[i];
+            result[i] = transactions[id];
+        }
+
+        return result;
+    }
+
+    function viewAdmin() view public returns (address) {
+        return (admin);
+    }
 }

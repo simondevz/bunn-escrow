aab90aa4fe258f0ff10f3a5fa44febb31d6a520e
Peter <obidaberechukwu@gmail.com>
2024-04-04 12:28:30 +0100

    put in my contact info

--- bunn_escrow/app/team/page.tsx
+++ bunn_escrow/app/team/page.tsx
@@ -1,15 +1,27 @@
-import TeamCard from "@/components/TeamCard"
+import TeamCard from "@/components/TeamCard";
 
 export default function Team() {
   return (
-    <main className='mt-4'>
-      <h2 className='text-center text-2xl font-semibold'>
-          Get in touch with us 
-      </h2> 
+    <main className="mt-4">
+      <h2 className="text-center text-2xl font-semibold">
+        Get in touch with us
+      </h2>
       <div className="mt-4 grid lg:grid-cols-2 gap-10">
-        <TeamCard twitterUrl="https://twitter.com/NmesomaHenry" portfolioUrl='https://github.com/IamHenryOkeke' phone='tel:+2347080194374' linkedInUrl='http://www.linkedin.com/in/henry-okeke-3176481a9' emailAddress="iamhenryokeke@gmail.com"/>
-        <TeamCard twitterUrl="https://twitter.com/HeyEmdeee" portfolioUrl='https://www.behance.net/heisemdee' phone='tel:+2348106970750' linkedInUrl='http://www.linkedin.com/in/mac-daniel-dokubo' emailAddress="dokubomacdaniel@gmail.com"/>
+        <TeamCard
+          twitterUrl="https://twitter.com/NmesomaHenry"
+          portfolioUrl="https://github.com/IamHenryOkeke"
+          phone="tel:+2347080194374"
+          linkedInUrl="http://www.linkedin.com/in/henry-okeke-3176481a9"
+          emailAddress="iamhenryokeke@gmail.com"
+        />
+        <TeamCard
+          twitterUrl="https://twitter.com/simondevz"
+          portfolioUrl="https://docs.google.com/document/d/1rBBk3FXI18sxh2FlYPNqI1J3Fk3cUOgHD960tYT0g5Q/edit?usp=sharing"
+          phone="tel:+2348157959120"
+          linkedInUrl="#"
+          emailAddress="obidaberechukwu@gmail.com"
+        />
       </div>
     </main>
-  )
+  );
 }
